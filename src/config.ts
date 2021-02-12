0df6ca376d3019dc69b0d77c70e6ec69d2a9df25
RikZun <rik.zunqq@gmail.com>
2021-02-12 04:12:12 +0300

    edit config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
-export const CLIENT_TOKEN = process.env.DISCORD_TOKEN ?? require('../config.json').CLIENT_TOKEN
-export const CLIENT_PREFIX = process.env.CLIENT_PREFIX ?? require('../config.json').CLIENT_PREFIX
-export const CLIENT_OWNER = process.env.CLIENT_OWNER ?? require('../config.json').CLIENT_OWNER
+export { CLIENT_TOKEN, CLIENT_PREFIX, CLIENT_OWNER }
+const CLIENT_TOKEN = process.env.DISCORD_TOKEN ?? require('../config.json').CLIENT_TOKEN
+const CLIENT_PREFIX = process.env.CLIENT_PREFIX ?? require('../config.json').CLIENT_PREFIX
+const CLIENT_OWNER = process.env.CLIENT_OWNER ?? require('../config.json').CLIENT_OWNER
